6e829faf352bbb90926107c522a11abf12482da5
Darmikon <hellfrom@mail.ru>
2017-07-15 02:02:00 +0300

    history api fix

--- src/middleware/history-api.js
+++ src/middleware/history-api.js
@@ -76,7 +76,7 @@ export default function historyApiFallback(options) {
     }
 
     if (Array.isArray(options.ignoredEndpoints)) {
-      options.ignoredEndpoints.some((str) => {
+      const match = options.ignoredEndpoints.some((str) => {
         if (parsedUrl.pathname.indexOf(str) !== -1) {
           logger(
             'Not rewriting',
@@ -84,9 +84,13 @@ export default function historyApiFallback(options) {
             req.url,
             'because it is ingored request.'
           );
-          return next();
+          return true;
         }
       });
+
+      if (match) {
+        return next();
+      }
     }
 
     rewriteTarget = options.index || '/index.html';
